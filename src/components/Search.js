3384639b1d60acb89dd8989573376a82c43dc8e5
claytonschoonoversmith@gmail.com <claytonschoonoversmith@gmail.com>
2020-12-18 15:54:05 -0600

    Search.js making wiki searches w/ useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
 	const [term, setTerm] = useState('');
-	
+
+	// console.log('I RUN WITH EVERY RENDER');
+
+	useEffect(() => {
+		const search = async () => {
+			await axios.get('https://en.wikipedia.org/w/api.php', {
+				params: {
+					action: 'query',
+					list: 'search',
+					origin: '*',
+					format: 'json',
+					srsearch: term
+				}
+			});
+		};
+
+		search();
+	}, [term]);
+
+
 	return(
 		<div>
 			<div className="ui form">
