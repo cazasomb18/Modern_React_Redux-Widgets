ad2e709a27f8736060080591fb9350b0479ddbd2
claytonschoonoversmith@gmail.com <claytonschoonoversmith@gmail.com>
2020-12-23 12:43:20 -0600

    completed convert/translate components, section 12 finished

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,8 +6,6 @@ const Search = () => {
 	const [debouncedTerm, setDebouncedTerm]  = useState(term);
 	const [results, setResults] = useState([]);
 
-	// console.log('RESULTS: ', results);
-
 	useEffect(() => {
 		const timerId = setTimeout(() => {
 			setDebouncedTerm(term);
