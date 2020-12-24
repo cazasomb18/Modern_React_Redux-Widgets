12240db11e452db32349d0f93a5c6ab87db4601f
claytonschoonoversmith@gmail.com <claytonschoonoversmith@gmail.com>
2020-12-24 12:25:05 -0600

    removing api key

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -26,7 +26,7 @@ const Convert = ({ language, text }) => {
 					params: {
 						q: debouncedText,
 						target: language.value,
-						key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+						key: 'see.env'
 					},
 				}
 			);
